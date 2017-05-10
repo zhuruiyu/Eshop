ed01a0d1926d1f0f375f00b8fd929ddf8a318b2b
zhuruiyu <harvey_zry@icloud.com>
2017-05-10 22:17:28 +0800

    product控制器实现首页数据加载和分页功能

---
+++ shan/application/models/product_model.php
@@ -0,0 +1,53 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Product_model extends CI_Model {
+
+	/*public function get_all_count()
+	{
+	    $this->db->select('product.*, img.img_src as prod_img');
+	    $this->db->from('t_product product');
+	    $this->db->join('t_product_img img', 'product.prod_id=img.prod_id');
+	    $this->db->where('img.is_main', 1);
+		return $this->db->count_all_results();
+	}
+    public function get_products($limit, $offset)
+    {
+        $this->db->select('product.*, img.img_src as prod_img');
+        $this->db->from('t_product product');
+        $this->db->join('t_product_img img', 'product.prod_id=img.prod_id');
+        $this->db->where('img.is_main', 1);
+        $this->db->limit($limit, $offset);
+        return $this->db->get()->result();
+    }*/
+
+    public function get_all_count($search)
+    {
+        $sql = "select product.*, img.img_src as prod_img from t_product product, t_product_img img where product.prod_id=img.prod_id and img.is_main=1";
+        if(isset($search['cate_id'])){
+            $sql = "SELECT prod.*,img.img_src as prod_img FROM (SELECT * FROM t_product WHERE cate_id IN
+                (SELECT cate_id
+                FROM t_category WHERE p_id=1
+                UNION
+                SELECT cate_id
+                FROM t_category WHERE p_id IN(SELECT cate_id FROM t_category WHERE p_id=1))) prod, t_product_img img
+                WHERE prod.prod_id=img.prod_id AND img.is_main=1";
+        }
+        return $this->db->query($sql)->num_rows();
+    }
+    public function get_products($limit, $offset, $search)
+    {
+
+        $sql = "select product.*, img.img_src as prod_img from t_product product, t_product_img img where product.prod_id=img.prod_id and img.is_main=1 limit $offset, $limit";
+
+        if(isset($search['cate_id'])){
+            $sql = "SELECT prod.*,img.img_src as prod_img FROM (SELECT * FROM t_product WHERE cate_id IN
+                (SELECT cate_id
+                FROM t_category WHERE p_id=1
+                UNION
+                SELECT cate_id
+                FROM t_category WHERE p_id IN(SELECT cate_id FROM t_category WHERE p_id=1))) prod, t_product_img img
+                WHERE prod.prod_id=img.prod_id AND img.is_main=1 LIMIT $offset, $limit";
+        }
+        return  $this->db->query($sql)->result();
+    }
+}
---
+++ shan/application/views/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,600,800,700,500,300,100,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Arimo:400,700,700italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body style="height: 2000px;">
+    <div id="header">
+        <div class="top-menu wrapper">
+            <a href="#" id="login">LOGIN</a>
+            <div id="cart-box">
+                <a href="#">
+                    Cart: <span id="money">0</span>(<span id="quantity"></span>)
+                </a>
+            </div>
+            <h1 id="logo">NEW FASHIONS</h1>
+        </div>
+        <ul id="nav" class="wrapper">
+            <li class="active"><a href="#">HOME</a></li>
+            <li>
+                <a href="#">WOMEN</a>
+                <div class="sub-menu">
+                    <div class="div col">
+                        <h4>item 1</h4>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="div col">
+                        <h4>item 2</h4>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="div col">
+                        <h4>item 3</h4>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="div col">
+                        <h4>item 4</h4>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="div col">
+                        <h4>item 5</h4>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li><a href="#">MEN</a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">BLOG</a></li>
+            <li><a href="#">SHOP ONLINE</a></li>
+        </ul>
+    </div>
+
+    <div id="product" class="wrapper">
+        <ul id="product-tags">
+            <li>Best seller</li>
+            <li>Popular</li>
+            <li>New Arrivals</li>
+        </ul>
+        <div id="loading">
+            <img src="img/loading.gif" alt="">
+        </div>
+        <ul id="product-list">
+            <!--<li><img src="uploads/products/bs1.jpg" alt="">
+                <div class="product-info">
+                    <h3 class="product-name">c</h3>
+                    <strong class="product-price">$187.95</strong>
+                    <input type="text" class="quantity" value="1">
+                    <button class="btn-add-cart">Add</button>
+                </div>
+            </li>
+            <li><img src="uploads/products/bs2.jpg" alt="">
+                <div class="product-info">
+                    <h3 class="product-name">CLUBYORK</h3>
+                    <strong class="product-price">$187.95</strong>
+                    <input type="text" class="quantity" value="1">
+                    <button class="btn-add-cart">Add</button>
+                </div>
+            </li>
+            <li><img src="uploads/products/bs3.jpg" alt="">
+                <div class="product-info">
+                    <h3 class="product-name">ROADSTER</h3>
+                    <strong class="product-price">$220.95</strong>
+                    <input type="text" class="quantity" value="1">
+                    <button class="btn-add-cart">Add</button>
+                </div>
+            </li>-->
+        </ul>
+        <div id="load-more">
+            加载更多...
+        </div>
+    </div>
+    <script id="product-tpl" type="text/html">
+        <li class="product-item">
+            <img src="{{img}}" alt="">
+            <div class="product-info">
+                <h3 class="product-name">{{name}}</h3>
+                <strong class="product-price">${{price}}</strong>
+                <input type="text" class="quantity" value="{{quantity}}">
+                <button class="btn-add-cart">Add</button>
+            </div>
+        </li>
+    </script>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/template.js"></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
